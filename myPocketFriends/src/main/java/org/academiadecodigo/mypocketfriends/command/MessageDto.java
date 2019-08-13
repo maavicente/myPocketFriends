c04503f5a1208213e4b8110028689f10185472f2
Reinaldo Pimentel <reinaldo.pimentel@ymail.com>
2019-08-13 21:18:18 +0000

    dto created

---
+++ myPocketFriends/src/main/java/org/academiadecodigo/mypocketfriends/command/MessageDto.java
@@ -0,0 +1,82 @@
+package org.academiadecodigo.mypocketfriends.command;
+
+import javax.validation.constraints.*;
+
+public class MessageDto {
+
+    private Integer id;
+
+    @NotNull(message = "Account number is mandatory")
+    private Integer accountNumber;
+
+    @NotNull(message = "Name is mandatory")
+    @NotBlank(message = "Name is mandatory")
+    @Size(min = 3, max = 64)
+    private String name;
+
+    @Email
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+
+    @Pattern(regexp = "^\\+?[0-9]*$", message = "Phone number contains invalid characters")
+    @Size(min = 9, max = 16)
+    private String phone;
+
+    private String description;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(Integer accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public String getPhone() {
+        return phone;
+    }
+
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+}
